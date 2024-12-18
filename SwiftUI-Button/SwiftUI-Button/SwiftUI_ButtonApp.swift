02ff863529507b5aa3385debac0d6ce122bc53df
Joel Crawford <joelcrawford@Joels-MacBook-Pro.local>
2024-12-18 12:08:11 +0300

    Initial Commit

---
+++ SwiftUI-Button/SwiftUI-Button/SwiftUI_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ButtonApp.swift
+//  SwiftUI-Button
+//
+//  Created by Joel Crawford on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-Button/SwiftUI-ButtonTests/SwiftUI_ButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ButtonTests.swift
+//  SwiftUI-ButtonTests
+//
+//  Created by Joel Crawford on 18/12/2024.
+//
+
+import Testing
+@testable import SwiftUI_Button
+
+struct SwiftUI_ButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
